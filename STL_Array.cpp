13b77b11a20501c8c8d47d013792c88db5e7a7d4
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2025-05-07 22:06:21 +0530

    Add files via upload

---
+++ STL_Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+	int basic[3]={1,2,3};
+	
+	array<int,4> a={1,2,3,4};  // array<datatype,size> variable_declaration ={array nos.}
+	
+	int size=a.size();
+	
+	for(int i=0;i<size;i++){
+		cout<<a[i]<<endl;
+	}
+	
+	cout<<"Element at 2nd Index :"<<a.at(2)<<endl;
+	cout<<"Empty or not :"<<a.empty()<<endl;
+	cout<<"First element :"<<a.front()<<endl;
+	cout<<"Last element :"<<a.back()<<endl;
+}
