e54c24b7b7b89f75e950220297aab9a603c42733
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2025-05-22 22:16:48 +0530

    Update DSA_String_C++.cpp

--- DSA_String_C++.cpp
+++ DSA_String_C++.cpp
@@ -3,7 +3,7 @@
 #include<string>
 using namespace std;
 int main(){
-	string s1="lskdfjXfkxL";  //phle capital letter print hunge fir small
+	string s1="lsjXfkxL";  //phle capital letter print hunge fir small
 	sort(s1.begin(),s1.end());
 	cout<<s1<<endl;
 }
