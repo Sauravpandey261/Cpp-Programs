cf836d31398dfa6c8df71bafd044ca185ef9a110
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2025-06-11 23:26:54 +0530

    Update sum of 3 no..cpp

--- sum of 3 no..cpp
+++ sum of 3 no..cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	int n1,n2,n3;
-	cout<<"enter the numbers:";
+	cout<<"enter the nos:";
 	cin>>n1>>n2>>n3;
 	cout<<"sum of "<<n1<< ","<<n2<<" and "<<n3<<" is "<<n1+n2+n3<<std::endl;
 	return 0;
